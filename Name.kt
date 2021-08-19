34acd72746c541b37278207ad7c71a7e66f3faac
Jupiter-Plantagenet <61136165+Jupiter-Plantagenet@users.noreply.github.com>
2021-08-19 09:54:22 +0100

    Add files via upload

---
+++ Name.kt
@@ -0,0 +1,10 @@
+//Where all functions run
+fun main() {
+    printName()
+}
+
+//Function prints the content of fullname variable when called
+private fun printName() {
+    val fullName = "George, of the House Akor. First of His name, the Prince that was promised"
+    print(fullName)
+}
